a11b319b6835bc820dd5db76a54bc47aca9b489f
RobStallion <Robertefrancis18@gmail.com>
2016-04-15 19:31:45 +0100

    add storeUser func

--- backend/lib/redis/redisFunctions.js
+++ backend/lib/redis/redisFunctions.js
@@ -2,5 +2,9 @@ export const getAllUsers = (client) => {
   return client.hgetallAsync('users')
 }
 
+export const storeUser = (client, username, data) => {
+  return client.hsetAsync('users', username, data)
+}
+
     })
 }
