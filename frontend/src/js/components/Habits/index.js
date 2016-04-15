f7590f0ce5004f9a532b77523638f339d6b86e8c
virginie <virginietrubiano@gmail.com>
2016-04-15 16:49:34 +0100

    add habit for in Habit Component

--- frontend/src/js/components/Habits/index.js
+++ frontend/src/js/components/Habits/index.js
@@ -8,8 +8,24 @@ export default class Habits extends Component {
         <h2>Habits</h2>
         <div id='add-habit'>
           <span>+ Add new habit</span>
+          <div className='add-habit-htmlForm'>
+            <label htmlFor='habit-name'>Label your Habit:</label>
+            <input name='habit-name'/> <br />
+            <label htmlFor='frequency'>Set the Frequency:</label>
+            <select name='frequency'>
+              <option value='daily'>daily</option>
+              <option value='every other day'>every other day</option>
+              <option value='week days'>week days</option>
+              <option value='weekly'>weekly</option>
+            </select> <br />
+            <label htmlFor='startDate'>Start Date:</label>
+            <input type='date' name='startDate'/> <br />
+            <label htmlFor='endDate'>End Date:</label>
+            <input type='date' name='endDate'/>
+          </div>
         </div>
         <div className='habits-list'>
+          <h3>Track your habit</h3>
           <ul>
             <li>first habit</li>
           </ul>
