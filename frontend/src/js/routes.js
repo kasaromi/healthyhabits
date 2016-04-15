c11e01dad1403279d3ef39033dc47f2b491bf07f
Katherine Bowler <katherine.bowler@gmail.com>
2016-04-15 16:01:39 +0100

    Changes default routes

--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import App from './components/App.js'
-import Home from './components/Home/index.js'
-import About from './components/About/index.js'
-import Contact from './components/Contact/index.js'
+import Login from './components/Login/'
+import Habits from './components/Habits/'
+import Rewards from './components/Rewards/'
 
 export default (
   <Route path='/' component={App}>
-    <IndexRoute component={Home} />
-    <Route path='/about' component={About} />
-    <Route path='/contact' component={Contact} />
+    <Route path='/login' component={Login} />
+    <Route path='/habits' component={Habits} />
+    <Route path='/rewards' component={Rewards} />
   </Route>
 )
