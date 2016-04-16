4305e38cb15e4c6bfb85d887ac3df0442d8b34b9
RobStallion <Robertefrancis18@gmail.com>
2016-04-16 16:20:08 +0100

    test addNewAction function

--- backend/tests/redis.tests.js
+++ backend/tests/redis.tests.js
@@ -5,7 +5,7 @@ const redis = require('redis')
 const bluebird = require('bluebird')
 
 const db = require('../dist/redis/redisFunctions.js')
-const helperFunctions = require('./redis.test.helper.js')
+// const helperFunctions = require('./redis.test.helper.js')
 
 bluebird.promisifyAll(redis.RedisClient.prototype)
 bluebird.promisifyAll(redis.Multi.prototype)
@@ -29,47 +29,9 @@ const test = tape({
   }
 })
 
-test('getAllUsers gets an array of all users', (t) => {
-  helperFunctions.setNusers(client, [1, 2, 3, 4, 5], () => {
-    db.getAllUsers(client).then((data) => {
-      const actual = Object.keys(data)
-      const expected = helperFunctions.nUsersArray([1, 2, 3, 4, 5])
-      t.deepEqual(actual, expected, 'worked!')
-      t.end()
-    })
-  })
-})
-
-test('checkUsernameAvalibility returns false if a user is not available', (t) => {
-  client.hsetAsync('users', 'katStallion', 'obj')
-  .then(() => {
-    db.checkUsernameAvalibility(client, 'katStallion')
-      .then(data => {
-        const actual = data
-        const expected = false
-        t.deepEqual(actual, expected, 'worked!')
-        t.end()
-      })
-  })
-})
-
-test('checkUsernameAvalibility returns false if a user is available', (t) => {
-  client.hsetAsync('users', 'katStallion', 'obj')
-  .then(() => {
-    db.checkUsernameAvalibility(client, 'robStallion')
-      .then(data => {
-        const actual = data
-        const expected = true
-        t.deepEqual(actual, expected, 'worked!')
-        t.end()
-      })
-  })
-})
-
 test('storeUser adds a new user with an empty habits array', (t) => {
   db.storeUser(client, 'Robstallion', JSON.stringify({
-    password: 'password1',
-    habit: []
+    actions: []
   }))
   .then(() => {
     db.getAllUsers(client).then(data => {
@@ -81,13 +43,12 @@ test('storeUser adds a new user with an empty habits array', (t) => {
   })
 })
 
-test('getUserHabits gets habits of user as an array', (t) => {
+test('getUserActions gets habits of user as an array', (t) => {
   client.hsetAsync('users', 'SamStallion', JSON.stringify({
-    password: 'password1',
-    habits: ['coding', 'running']
+    actions: ['coding', 'running']
   }))
   .then(() => {
-    db.getUserHabits(client, 'SamStallion')
+    db.getUserActions(client, 'SamStallion')
     .then((results) => {
       t.deepEqual(results, ['coding', 'running'])
       t.end()
@@ -95,19 +56,19 @@ test('getUserHabits gets habits of user as an array', (t) => {
   })
 })
 
-// // test('addNewHabit adds a new habit to the habit array for an already set up user', (t) => {
-// //   client.hset('users', 'user1', {
-// //     password: 'password1',
-// //     habits: []
-// //   }).then(() => {
-// //     return db.getPrechatNotes(client, 'john')
-// //   }).then((results) => {
-// //     t.deepEqual(results, [{
-// //       menteeName: 'john',
-// //       mentorName: 'sally',
-// //       note: 'Some stuff',
-// //       date: '2016/04/01'
-// //     }])
-// //     t.end()
-// //   })
-// // })
+test('addNewAction adds a new action to the array of existing actions', (t) => {
+  client.hsetAsync('users', 'SamStallion', JSON.stringify({
+    actions: ['coding', 'running']
+  }))
+  .then(() => {
+    db.addNewAction(client, 'SamStallion', 'swimming')
+    .then(() => {
+      client.hgetAsync('users', 'SamStallion')
+        .then(data => {
+          const results = JSON.parse(data)
+          t.deepEqual(results, ['coding', 'running', 'swimming'])
+          t.end()
+        })
+    })
+  })
+})
